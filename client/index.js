b55b352c081d747c87ac37e0333911423a37dcec
Corey <cry@Coreys-MacBook-Air.local>
2018-01-22 14:51:03 -0800

    Initialize basic skeleton

---
+++ client/index.js
@@ -0,0 +1,15 @@
+// DOM manipulation logic
+
+getString = () => {
+  // Get string from the form
+}
+
+fetch(''/* local server */, {
+  // use getString for request body
+	method: 'get'
+}).then(function(response) {
+  // append to html table on index page
+}).catch(function(err) {
+	// Error :(
+});
+
